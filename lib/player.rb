2ff1aa495a179c074ae78f06473ddad4cd278009
Felix Mckenzie <felixmckenzie90@gmail.com>
2022-04-17 23:08:07 +1000

    added player class to player.rb

---
+++ lib/player.rb
@@ -0,0 +1,16 @@
+require 'tty-prompt'
+
+
+class User
+    attr_accessor :username, 
+
+    def initialize (username)
+        @username = username
+        @bank_account = []
+    end
+
+    
+
+end
+
+
